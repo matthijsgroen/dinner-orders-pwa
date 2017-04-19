7d9068d1a2064bb6124964077bec8389e50c3007
Pascal Widdershoven <pascal@kabisa.nl>
2017-04-19 07:45:46 +0200

    Prettify webpack config and deploy-to-s3 script

--- scripts/deploy-to-s3.js
+++ scripts/deploy-to-s3.js
@@ -1,31 +1,37 @@
-var s3 = require('s3');
-require('dotenv').config();
+var s3 = require("s3");
+require("dotenv").config();
 
 var client = s3.createClient({
   s3Options: {
     accessKeyId: process.env.AWS_ACCESS_KEY,
-    secretAccessKey: process.env.AWS_SECRET_KEY,
-  },
+    secretAccessKey: process.env.AWS_SECRET_KEY
+  }
 });
 
 var params = {
-  localDir: 'dist/',
+  localDir: "dist/",
   deleteRemoved: true,
 
   s3Params: {
     Bucket: process.env.AWS_BUCKET,
     Prefix: process.env.AWS_PATH,
-    ACL: 'public-read'
-  },
+    ACL: "public-read"
+  }
 };
 
 var uploader = client.uploadDir(params);
-uploader.on('error', function(err) {
-  console.error('unable to sync:', err.stack);
+uploader.on("error", function(err) {
+  console.error("unable to sync:", err.stack);
 });
-uploader.on('progress', function() {
-  process.stdout.write('Progress ' + uploader.progressAmount + ' of ' + uploader.progressTotal + '\r')
+uploader.on("progress", function() {
+  process.stdout.write(
+    "Progress " +
+      uploader.progressAmount +
+      " of " +
+      uploader.progressTotal +
+      "\r"
+  );
 });
-uploader.on('end', function() {
-  console.log('\ndone uploading');
+uploader.on("end", function() {
+  console.log("\ndone uploading");
 });
--- webpack.config.js
+++ webpack.config.js
@@ -1,93 +1,106 @@
 const path = require("path");
 
 const babel = require("./config/babel");
-const uglify = require('./config/uglify');
+const uglify = require("./config/uglify");
 
 //** constants **//
 
-const env = process.env.NODE_ENV || 'development';
-const isProd = (env === 'production');
+const env = process.env.NODE_ENV || "development";
+const isProd = env === "production";
 const out = path.join(__dirname, "./dist");
 const exclude = /(node_modules|bower_components)/;
 
 //** webpack plugins **//
 
-const webpack = require('webpack');
-const OfflinePlugin = require('offline-plugin');
-const Clean = require('clean-webpack-plugin');
-const Copy = require('copy-webpack-plugin');
-const HTML = require('html-webpack-plugin');
-const Dotenv = require('dotenv-webpack');
+const webpack = require("webpack");
+const OfflinePlugin = require("offline-plugin");
+const Clean = require("clean-webpack-plugin");
+const Copy = require("copy-webpack-plugin");
+const HTML = require("html-webpack-plugin");
+const Dotenv = require("dotenv-webpack");
 
 const ExtractText = require("extract-text-webpack-plugin");
 const extractShellCss = new ExtractText("shell.[hash].css");
 const extractOtherCss = new ExtractText("styles.[hash].css");
 
 const plugins = [
-    new Dotenv({systemvars: true}),
-    new Clean(['dist']),
-    new webpack.optimize.CommonsChunkPlugin({name: 'vendor'}),
-    new webpack.DefinePlugin({'process.env.NODE_ENV': JSON.stringify(env)}),
-    new HTML({template: 'src/index.html', inject: false, minify: isProd ? {
-        removeComments: true,
-        collapseWhitespace: true
-      } : false}),
-    new HTML({template: 'src/static/manifest.json', filename: 'manifest.json', minify: false, inject: false}),
-    new webpack.LoaderOptionsPlugin({
-        options: {
-            postcss: [
-                require('autoprefixer')({
-                    browsers: ['last 3 Chrome versions', 'last 3 iOS versions', 'last 3 Edge versions']
-                })
-            ]
+  new Dotenv({ systemvars: true }),
+  new Clean(["dist"]),
+  new webpack.optimize.CommonsChunkPlugin({ name: "vendor" }),
+  new webpack.DefinePlugin({ "process.env.NODE_ENV": JSON.stringify(env) }),
+  new HTML({
+    template: "src/index.html",
+    inject: false,
+    minify: isProd
+      ? {
+          removeComments: true,
+          collapseWhitespace: true
         }
-    }),
-    extractShellCss,
-    extractOtherCss
+      : false
+  }),
+  new HTML({
+    template: "src/static/manifest.json",
+    filename: "manifest.json",
+    minify: false,
+    inject: false
+  }),
+  new webpack.LoaderOptionsPlugin({
+    options: {
+      postcss: [
+        require("autoprefixer")({
+          browsers: [
+            "last 3 Chrome versions",
+            "last 3 iOS versions",
+            "last 3 Edge versions"
+          ]
+        })
+      ]
+    }
+  }),
+  extractShellCss,
+  extractOtherCss
 ];
 
 if (isProd) {
-    plugins.push(
-        new webpack.LoaderOptionsPlugin({minimize: true, debug: false}),
-        new webpack.optimize.UglifyJsPlugin(uglify),
-        new OfflinePlugin({
-          autoUpdate: true,
-          relativePaths: true,
-          rewrites: { "/": "/index.html" },
-          ServiceWorker: {
-            output: 'service-worker.js',
-            navigateFallback: "index.html",
-            events: true
-          },
-          AppCache: {
-            events: true
-          }
-        })
-    );
+  plugins.push(
+    new webpack.LoaderOptionsPlugin({ minimize: true, debug: false }),
+    new webpack.optimize.UglifyJsPlugin(uglify),
+    new OfflinePlugin({
+      autoUpdate: true,
+      relativePaths: true,
+      rewrites: { "/": "/index.html" },
+      ServiceWorker: {
+        output: "service-worker.js",
+        navigateFallback: "index.html",
+        events: true
+      },
+      AppCache: {
+        events: true
+      }
+    })
+  );
 
-    babel.presets.push("babili");
+  babel.presets.push("babili");
 } else {
-    // dev only
-    plugins.push(
-        new webpack.HotModuleReplacementPlugin(),
-        new webpack.NamedModulesPlugin()
-    );
+  // dev only
+  plugins.push(
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NamedModulesPlugin()
+  );
 }
 
 module.exports = {
-    entry: {
-        app: "./src/index.js",
-        vendor: [
-            "preact"
-        ]
-    },
-    output: {
-        path: out,
-        filename: "[name].[hash].js",
-        publicPath: "./"
-    },
-    module: {
-        rules: [
+  entry: {
+    app: "./src/index.js",
+    vendor: ["preact"]
+  },
+  output: {
+    path: out,
+    filename: "[name].[hash].js",
+    publicPath: "./"
+  },
+  module: {
+    rules: [
       {
         test: /\.jsx?$/,
         exclude: exclude,
@@ -96,33 +109,37 @@ module.exports = {
       },
       {
         test: /\.scss$/,
-        loader: isProd ? extractOtherCss.extract({
-          use: "css-loader?modules!postcss-loader!sass-loader"
-        }) : "style-loader!css-loader?modules!postcss-loader!sass-loader",
+        loader: isProd
+          ? extractOtherCss.extract({
+              use: "css-loader?modules!postcss-loader!sass-loader"
+            })
+          : "style-loader!css-loader?modules!postcss-loader!sass-loader",
         exclude: /shell.scss/
       },
       {
         test: /shell.scss$/,
-        loader: isProd ? extractShellCss.extract({
-          use: "css-loader!postcss-loader!sass-loader"
-        }) : "style-loader!css-loader!postcss-loader!sass-loader"
+        loader: isProd
+          ? extractShellCss.extract({
+              use: "css-loader!postcss-loader!sass-loader"
+            })
+          : "style-loader!css-loader!postcss-loader!sass-loader"
       }
     ]
-    },
-    resolve: {
-      alias: {
-        "src": path.resolve(__dirname, "./src")
-      }
-    },
-    plugins: plugins,
-    devtool: !isProd && "eval",
-    devServer: {
-    publicPath: "/",
-        contentBase: out,
-        port: process.env.PORT || 3000,
-        historyApiFallback: true,
-        compress: isProd,
-        inline: !isProd,
-        hot: !isProd
+  },
+  resolve: {
+    alias: {
+      src: path.resolve(__dirname, "./src")
     }
+  },
+  plugins: plugins,
+  devtool: !isProd && "eval",
+  devServer: {
+    publicPath: "/",
+    contentBase: out,
+    port: process.env.PORT || 3000,
+    historyApiFallback: true,
+    compress: isProd,
+    inline: !isProd,
+    hot: !isProd
+  }
 };
