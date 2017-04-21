80f3c9a1d6dc8917b9c9395f222fa2ad2f46b934
Matthijs Groen <matthijs.groen@gmail.com>
2017-04-21 10:29:42 +0200

    Add test runner

---
+++ karma.conf.js
@@ -0,0 +1,34 @@
+const webpackConfig = require("./webpack.config.js");
+
+const commonsChunkPluginIndex = webpackConfig.plugins.findIndex(plugin => plugin.chunkNames);
+webpackConfig.plugins.splice(commonsChunkPluginIndex, 1);
+
+module.exports = function(config) {
+  config.set({
+    basePath: '',
+    frameworks: ['mocha', 'chai', 'sinon'],
+    files: [
+      'spec/**/*_spec.js'
+    ],
+    exclude: [
+    ],
+    preprocessors: {
+      "spec/**/*_spec.js": ["webpack"]
+    },
+    // webpack configuration
+    webpack: webpackConfig,
+    webpackMiddleware: {
+      stats: "errors-only"
+    },
+    reporters: ['mocha'],
+    port: 9876,
+    colors: true,
+    logLevel: config.LOG_INFO,
+    autoWatch: true,
+    browsers: ['PhantomJS'],
+    // Continuous Integration mode
+    // if true, Karma captures browsers, runs the tests and exits
+    singleRun: false,
+    concurrency: Infinity
+  })
+}
